b509061da8719a956dce244f959f65e15476eb7f
Ángel Hurtado <angelhurtado27c@gmail.com>
2025-02-11 16:55:34 -0500

    Agregando evento para conversion de divisa mediante un hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import {useRef} from 'react'
 import './App.css'
 
 function App() {
+  const cambio = 23.10
+  const refValor = useRef()
+
   function incrementar(e) {
     const tag = e.target
     tag.innerText = Number(tag.innerText) + 1
   }
 
+  function convertir() {
+    refValor.current.innerText *= cambio
+  }
+
   return <>
     <h1>Eventos</h1>
-    <p className="contador" onClick={incrementar}>0</p>
-    <button>Incrementar</button>
+    <p ref={refValor} className="contador" onClick={incrementar}>0</p>
+    <button onClick={convertir}>Incrementar</button>
   </>
 }
 
