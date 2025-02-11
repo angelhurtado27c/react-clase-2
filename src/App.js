c497d864bb026c04f6b4fe4cc38631833f6b914d
Ángel Hurtado <angelhurtado27c@gmail.com>
2025-02-11 17:34:16 -0500

    Cambiando funciones normales por funciones de flecha

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ function App() {
   const cambio = 23.10
   const refValor = useRef()
 
-  function incrementar(e) {
+  const incrementar = e => {
     const tag = e.target
     tag.innerText = Number(tag.innerText) + 1
   }
 
-  function convertir() {
+  const convertir = () => {
     refValor.current.innerText *= cambio
+  }
+
+  const inputCambio = e => {
+      refValor.current.innerText = e.target.value
   }
 
   return <>
     <h1>Eventos</h1>
     <p ref={refValor} className="contador" onClick={incrementar}>0</p>
     <button onClick={convertir}>Incrementar</button>
+    <br/>
+    <input onChange={inputCambio}/>
   </>
 }
 
