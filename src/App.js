e19a8303d53b1afd261d46faeb1774b43f5df4f2
Ángel Hurtado <angelhurtado27c@gmail.com>
2025-02-14 19:38:50 -0500

    Agregando evento para cambio de imagen

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import img_0 from './img-reyes/rey_atanagildo.png'
+import img_1 from './img-reyes/rey_sisebuto.png'
 import {useRef} from 'react'
 import './App.css'
 
@@ -18,8 +20,25 @@ function App() {
       refValor.current.innerText = e.target.value
   }
 
+  let i_img = 0
+  const cambiarImagen = e => {
+    let img
+    switch (i_img) {
+      case 0:
+        img = img_1
+        i_img = 1
+        break
+      case 1:
+        img = img_0
+        i_img = 0
+        break
+    }
+    e.target.src = img
+  }
+  
   return <>
     <h1>Eventos</h1>
+    <img src={img_0} onClick={cambiarImagen} />
     <p ref={refValor} className="contador" onClick={incrementar}>0</p>
     <button onClick={convertir}>Incrementar</button>
     <br/>
